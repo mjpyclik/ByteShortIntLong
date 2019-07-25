1efaa00be57598cf6ebdc71815b3cdf6cc671824
mpyclik <mjpyclik@gmail.com>
2019-07-24 19:55:55 -0500

    Created a file

---
+++ src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int myValue = 10000;
+
+        int maxInt = Integer.MAX_VALUE;
+        int minInt = Integer.MIN_VALUE;
+
+        System.out.println("Minimum Int value " + minInt);
+        System.out.println("Maximum Int value " + maxInt);
+    }
+}
